149449cc169f0bdede517c3a7b5a7749ef1b5561
ted_scanlan <t.scanlan92@gmail.com>
2019-11-15 16:44:13 +0000

    refactoring and readme updated

--- src/program.js
+++ src/program.js
@@ -1,47 +1,24 @@
 const { Hoover } = require('./hoover')
 
-
-
-
-
-
-
-//
-// function runProgram(input) {
-//   console.log(input);
-//   const directions = getDirections(input)
-//   hoover = new Hoover(input)
-//   for(var i = 0; i <= directions.length-1; i++) {
-//   hoover.processMove(directions[i]);
-//   }
-//   let result = hoover.outcome();
-//     console.log(result);
-//     return result
-//   }
-
 class Program {
-constructor(instructions, hoover = Hoover) {
-  console.log(instructions);
-  this.hoover = new hoover(instructions),
-  this.directions = this.getDirections(instructions);
-}
-
-run() {
-  for(var i = 0; i <= this.directions.length-1; i++) {
-    this.hoover.processMove(this.directions[i]);
+  constructor(instructions, hoover = Hoover) {
+    this.hoover = new hoover(instructions),
+    this.directions = this.getDirections(instructions);
   }
-  let result = this.hoover.outcome();
-  return result
-}
+
+  run() {
+    for(var i = 0; i <= this.directions.length-1; i++) {
+      this.hoover.processMove(this.directions[i]);
+    }
+    let result = this.hoover.outcome();
+    return result
+  }
 
   getDirections(input) {
-  return input[input.length-2].split("")
-}
+    return input[input.length-2].split("")
+  }
 
 }
-
-
-
 
 
   module.exports = {
