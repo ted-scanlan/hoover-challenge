93e8465d2491f7d9781398bebf141d05f7a638f2
ted_scanlan <t.scanlan92@gmail.com>
2019-11-15 15:19:46 +0000

    readme added

--- tests/feature/program.test.js
+++ tests/feature/program.test.js
@@ -1,8 +1,12 @@
 const { Program } = require('../../src/program');
 
-let double = [ '5 5', '1 2', '1 0', '2 2', '2 3', 'NNESEESWNWW', '' ]
+let mock_instructions = [ '5 5', '1 2', '1 0', '2 2', '2 3', 'NNESEESWNWW', '' ]
+let mock_instructions2 = [ '5 5', '1 2', '1 0', '2 2', '2 3', 'NNNNNNNNN', '' ]
 
-test('should remove nulls from list of languages', () => {
-
-const program = new Program(double)
+test('return the correct outcome for given input', () => {
+const program = new Program(mock_instructions)
 expect(program.run()).toEqual("1 3\n1")})
+
+test('it stays within boundaries', () => {
+const program = new Program(mock_instructions2)
+expect(program.run()).toEqual("1 4\n0")})
